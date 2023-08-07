1b1e97e97d1b9ee0025155991004274a814bfc31
citec-47 <mauricendonyi40@gmail.com>
2023-08-07 03:38:49 -0700

    Sign in, sign out, create categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "categories#index"
+  
+  resources :categories do 
+  resources :expenditures do 
+    resources :category_expenditures
+  end
+  end 
+  resources :users
+
 end
