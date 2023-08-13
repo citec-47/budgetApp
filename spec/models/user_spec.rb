e491fd43219befb0f6dfd5c57a2622945b989435
citec-47 <mauricendonyi40@gmail.com>
2023-08-13 04:33:23 -0700

    F

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,24 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  before(:all) do
-    @user_one = User.new(name: 'anye', email: 'prteter@gmail.com', password: 12_345_678,
-                         password_confirmation: 12_345_678)
-  end
-  context 'validations' do
-    it 'is valid with valid attributes' do
-      expect(@user_one).to be_valid
-    end
-    it 'is not valid without a name' do
-      @user_one.name = ''
-      expect(@user_one).to_not be_valid
-    end
-    it 'Is should valid the post counter' do
-      @user_one.email = ''
-      expect(@user_one).to_not be_valid
-    end
-  end
-end
+# RSpec.shared_examples "invalid_when_attribute_is_blank" do |attribute|
+#   it "is not valid without #{attribute}" do
+#     @user_one[attribute] = ''
+#     expect(@user_one).not_to be_valid
+#   end
+# end
+
+# RSpec.describe User, type: :model do
+#   before(:all) do
+#     @user_one = User.new(name: 'Ndonyi Maurice', email: 'mauricendonyi40@gmail.com', password: 12_345_678,
+#                          password_confirmation: 12_345_678)
+#   end
+  
+#   context 'validations' do
+#     it 'is valid with valid attributes' do
+#       expect(@user_one).to be_valid
+#     end
+    
+#     it_behaves_like "invalid_when_attribute_is_blank", :name
+#     it_behaves_like "invalid_when_attribute_is_blank", :email
+#   end
+# end
